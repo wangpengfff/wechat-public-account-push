553fe279da26a1f3999cee2797bde1ae97af8914
wangpengfff <59726044+wangpengfff@users.noreply.github.com>
2022-10-04 18:22:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ow3tI6-N7PX3cfNA2PfutWU-KPJY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Fjzx5uhkluwUXqHSdl5BQRmWvOuoWka38f7nE2-6AGM	',
+      useTemplateId: 'Fjzx5uhkluwUXqHSdl5BQRmWvOuoWka38f7nE2-6AGM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '5-21',
       festivals: [
