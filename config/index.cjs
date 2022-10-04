00e4dde700383ea048931bcd8149065f2d95df2e
wangpengfff <59726044+wangpengfff@users.noreply.github.com>
2022-10-04 18:30:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝宝',
+      name: '成一平',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ow3tI6-N7PX3cfNA2PfutWU-KPJY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Fjzx5uhkluwUXqHSdl5BQRmWvOuoWka38f7nE2-6AGM',
+      useTemplateId: 'icP5elCBRvlMYGHLau0i9Q8Zb6sQeIrVW2ekfI2eLZY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '5-21',
       festivals: [
@@ -36,7 +36,7 @@ const USER_CONFIG = {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '09-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
